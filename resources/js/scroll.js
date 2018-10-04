6557b711d5c87b3e5e736f1fa29712e4672632bb
Joshua Gretton <jb.gretton@googlemail.com>
2018-10-04 18:27:24 +0100

    Updates

---
+++ resources/js/scroll.js
@@ -0,0 +1,1 @@
+$('a[href*="#"]:not([href="#"]):not([href="#show"]):not([href="#hide"])').click(function(){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){var e=$(this.hash);if((e=e.length?e:$("[name="+this.hash.slice(1)+"]")).length)return $("html,body").animate({scrollTop:e.offset().top},1e3),!1}});
